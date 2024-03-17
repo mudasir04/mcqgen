e02b8594f4b88a2257086ae55a27e5fa51ef69dc
mudasir04 <mudasir04@gmail.com>
2024-03-17 22:46:55 +0530

    update

--- StreamLitAPP.py
+++ StreamLitAPP.py
@@ -5,7 +5,8 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
-from langchain.callbacks import get_openai_callback
+from langchain_community.callbacks import get_openai_callback
+#from langchain.callbacks import get_openai_callback
 from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -5,25 +5,24 @@ import pandas as pd
 import traceback
 
 
-
 def read_file(file):
     if file.name.endswith(".pdf"):
         try:
             pdf_reader=PyPDF2.PdfFileReader(file)
-            text=""
+            text = ""
             for page in pdf_reader.pages:
                 text+=page.extract_text()
             return text
-            
-        except Exception as e:
-            raise Exception("error reading the PDF file")
         
-    elif file.name.endswith(".txt"):
+        except Exception as e:
+            raise Exception("error reading in the PDF file")
+    
+    elif file .name.endswith(".txt"):
         return file.read().decode("utf-8")
 
-    else:
-        raise Exception("unsupported file format only pdf and text file supported")        
-    
+    else: 
+        raise Exception("unsupported file format only pdf and text file supported")
+            
 
 def get_table_data(quiz_str):
     try:
