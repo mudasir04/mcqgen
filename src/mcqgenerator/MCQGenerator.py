d49b17868119d1fc660073af937137ec1f0a50f6
mudasir04 <mudasir04@gmail.com>
2024-03-17 21:27:11 +0530

    update

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
 from src. mcqgenerator.logger import logging
 
-
+from langchain_openai import ChatOpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
